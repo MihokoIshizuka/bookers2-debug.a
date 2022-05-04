5e6dacf96513bf623bd3aa22f8e3d854af6c6897
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-04 05:19:18 +0000

    [finish]7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
@@ -8,7 +9,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
